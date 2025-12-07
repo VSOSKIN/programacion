cc14f472f902c282e0a36488644c16303e88977e
Victoria.soskin <soskin1993@gmail.com>
2025-12-07 15:49:59 -0300

    C:\Users\buend\OneDrive\Desktop\programacion\proyectonex

--- ejemplos clases/05-for.js
+++
@@ -1,8 +0,0 @@
-const frutas =['bananas', 'manzanas', 'paltas', 'naranjas', 'mandarinas'];
-//console.log(frutas)
-console.log(frutas[2]) //paltas (porque el indice empieza en 0))
-console.log(frutas.length) //5 
-
-for (let i = 0 ; i < frutas.length; i++){
-console.log("tenemos " + frutas[i] + "")
-} 
