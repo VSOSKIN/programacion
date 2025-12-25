f7eb3e3f59ffcc3ba3fbcc060d7f66b8a95dd8a9
Victoria.soskin <soskin1993@gmail.com>
2025-12-25 19:44:44 -0300

    unidad4.tarea

--- cangoopetnext/src/app/page.js
+++ cangoopetnext/src/app/page.js
@@ -12,22 +12,41 @@ import { IMAGES_MANIFEST } from "next/dist/shared/lib/constants";
 
 export default function Home() {
   return (
-    <div className="columnas">
-      <div className="bienvenidos">
-        <h2>Bienvenidos a KangooPet</h2>
-        <p>Tu tienda online de confianza para todo lo relacionado con tus mascotas. En KangooPet, nos apasiona el bienestar de los animales y nos dedicamos a ofrecerte una amplia gama de productos de alta calidad para perros y gatos.</p>
-      </div>
-      <div className="fotoportada">
-        <Image src="/fotos/imagen1.webp" alt="Foto de mascotas" width={1100} height={300} />
-        <Image src="/fotos/imagen2.webp" alt="Foto de mascotas" width={1100} height={300} />
-        <Image src="/fotos/imagen3.webp" alt="Foto de mascotas" width={1100} height={300} />
-        <Image src="/fotos/imagen4.webp" alt="Foto de mascotas" width={1100} height={300} />
-      </div>
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/fotos/imagen1.webp" class="d-block w-100" alt="..."/>
     </div>
-
+    <div class="carousel-item">
+      <img src="/fotos/imagen2.webp" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="/fotos/imagen3.webp" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
   )
 };
 
+
+    //<div className="columnas">
+      //<div className="bienvenidos">
+      //</div>
+      //<div className="fotoportada">
+        //<Image src="/fotos/imagen1.webp" alt="Foto de mascotas" width={1100} height={300} />
+        //<Image src="/fotos/imagen2.webp" alt="Foto de mascotas" width={1100} height={300} />
+        //<Image src="/fotos/imagen3.webp" alt="Foto de mascotas" width={1100} height={300} />
+        //<Image src="/fotos/imagen4.webp" alt="Foto de mascotas" width={1100} height={300} />
+      //</div>
+    //</div>
 
 
 
